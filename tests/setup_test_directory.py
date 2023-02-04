4a4feaa734384285b5812504589bd264deac2196
yenaras <brandon82890@gmail.com>
2023-02-04 14:12:47 -0500

    add setup for easier manual testing and demo

---
+++ tests/setup_test_directory.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+def main():
+    setup_test_directory()
+
+
+def setup_test_directory():
+    os.makedirs("test_directory/SUB DIRECTORY/SUB SUB DIRECTORY", exist_ok=True)
+    with open("test_directory/TEST FILE.txt", "w") as f:
+        f.write("test")
+    with open("test_directory/SUB DIRECTORY/SUB FILE.txt", "w") as f:
+        f.write("test")
+    with open(
+        "test_directory/SUB DIRECTORY/SUB SUB DIRECTORY/SUB SUB FILE.txt", "w"
+    ) as f:
+        f.write("test")
+
+
+if __name__ == '__main__':
+    main()
